d8aeddb0a263ab99c91c2e63640a3ee0e1b9bae2
Padmaja-mam <83027479+Padmaja-mam@users.noreply.github.com>
2023-01-27 17:53:14 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -19,5 +19,36 @@ function gotResults(error, results)
   else
    {
     console.log(results);
+
+    document.getElementById("result_label").innerHTML = 'Sound : '+ results[0].label;
+    document.getElementById("result_confidence").innerHTML = 'Accuracy : '+ (results[0].confidence*100).toFixed(2)+" %";
+    
+    img = document.getElementById('sound1');
+
+    if(results[0].label == "dog")
+    {
+      img.src = 'dog1.gif';
+    }
+
+    else if(results[0].label == "cat")
+    {
+      img.src = 'Cat1.gif';
+    }
+
+    else if(results[0].label == "crow")
+    {
+      img.src = 'crow.gif';
+    }
+
+    else if(results[0].label == "cock")
+    {
+      img.src = 'hen.gif';
+    }
+
+    else if(results[0].label == "Background Noise")
+    {
+      img.src = 'bgnoise.png';
+    }
+
   }
 }
